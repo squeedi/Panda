d6fc437e07a688caad9f33110d95cdf47385fdf2
squee_di <masinovskaa@gmail.com>
2025-10-28 21:19:00 +0300

    Добавление

--- src/components/InstagramSection.jsx
+++ src/components/InstagramSection.jsx
@@ -2,9 +2,9 @@ import './InstagramSection.css';
 
 export default function InstagramSection() {
   const instagramImages = [
-    '{`${process.env.PUBLIC_URL}/assets/inst1.jpg`}',
-   '{`${process.env.PUBLIC_URL}/assets/inst2.jpg`}',
-   '{`${process.env.PUBLIC_URL}/assets/inst3.jpg`}',
+    '../assets/inst1.jpg',
+    '../assets/inst2.jpg',
+    '../assets/inst3.jpg',
   ];
 
   return (
